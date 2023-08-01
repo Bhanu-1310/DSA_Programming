2c2d4c1627a33d4c32b6282060fab0e520d16df8
Bhanu Arora <bhanuarora2506@gmail.com>
2023-08-01 12:13:14 +0530

    Add two numbers

--- b.cpp
+++ b.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
-    cout<<"fifth commit";
+    int x, y;
+int sum;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+sum = x + y;
+cout << "Sum is: " << sum;
 }
