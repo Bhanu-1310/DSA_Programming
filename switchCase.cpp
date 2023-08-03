08813c27c8070efad90f77e7611324836b6fd986
Bhanu Arora <bhanuarora2506@gmail.com>
2023-08-03 12:34:54 +0530

    eight

---
+++ switchCase.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+	int n = 2;
+
+	// Switch Case Statement
+	switch (n) {
+	case 1:
+		cout << "Computer Network"
+			<< endl;
+		break;
+	case 2:
+		cout << "C++" << endl;
+		break;
+	case 3:
+		cout << "DBMS" << endl;
+		break;
+	case 4:
+		cout << "Data Structure"
+			<< endl;
+		break;
+	case 5:
+		cout << "Operating System"
+			<< endl;
+		break;
+	default:
+		cout << "Enter Valid number"
+			<< endl;
+	}
+
+	return 0;
+}
