fc19b12a1dcb15c45c4f4c7569c9a3c33f52c824
Bhanu Arora <bhanuarora2506@gmail.com>
2023-08-06 11:12:32 +0530

    30

--- Operations.cpp
+++ Operations.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int findOdd(int arr[], int n)
-{
-	int res = 0, i;
-	for (i = 0; i < n; i++)
-		res ^= arr[i];
-	return res;
-}
+int main() {
 
-// Driver Method
-int main(void)
-{
-	int arr[] = { 12, 12, 14, 90, 14, 14, 14 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "The odd occurring element is "<< findOdd(arr, n);
+	int x = 19;
+	cout<<"x << 1 = "<< (x << 1) <<endl;
+	cout<<"x >> 1 = "<< (x >> 1) <<endl;
 	return 0;
 }
