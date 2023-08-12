7762b1af2aad814ec20f3e645c18ddb25e608779
Bhanu Arora <bhanuarora2506@gmail.com>
2023-08-12 23:32:54 +0530

    29

--- Operations.cpp
+++ Operations.cpp
@@ -1,11 +1,19 @@
-
 #include <iostream>
 using namespace std;
 
-int main()
+int findOdd(int arr[], int n)
 {
-	cout << "Size of double: " << sizeof(double) << endl;
-	cout << "Size of int: " << sizeof(int) << endl;
+	int res = 0, i;
+	for (i = 0; i < n; i++)
+		res ^= arr[i];
+	return res;
+}
 
+// Driver Method
+int main(void)
+{
+	int arr[] = { 12, 12, 14, 90, 14, 14, 14 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "The odd occurring element is "<< findOdd(arr, n);
 	return 0;
 }
